4a34bf63550608420226a2e39412709311755501
6gDav <c6gdavprog0402@gmail.com>
2026-09-17 20:39:33 +0200

    style: a simple clean style added to the error handler

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,5 +2,33 @@
   import {page} from '$app/state';
 </script>
 
+<div id="error-conatiner">
+  <h1>{page.status}</h1>
+  <hr>
+  <p>{page?.error?.message}</p>
+</div>
 
-<p style="color:red">{page.status}: {page?.error?.message}</p>
+<style>
+  #error-conatiner {
+    color: red;
+    display: grid;
+    /* justify-content: center; */
+    /* align-items: center; */
+    place-content: center;
+    min-height: 100vh;
+    text-align: center;
+    gap: 0rem;
+  }
+
+  #error-conatiner hr {
+    width: 100%;
+  }
+
+  #error-conatiner h1 {
+    font-size: 6rem;
+  }
+
+  #error-conatiner p {
+    font-size: 6rem;
+  }
+</style>
