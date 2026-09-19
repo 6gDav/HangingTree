664e48cf27150e658fb721005d649c643f04882b
Redstone <vamosattila3@gmail.com>
2026-09-19 14:40:03 +0200

    feat: words list and word selector, with a debug message on the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
+<script lang="ts">
+    const words: string[] = [
+        "programming",
+        "typescript",
+        "svelte",
+        "javascript",
+        "frontend",
+        "backend",
+        "developer",
+        "algorithm",
+        "function",
+        "variable"
+    ];
+
+    let selected_word: string = words[Math.floor(Math.random() * words.length)];
+</script>
+
 <h1 style="font-size: 55px;">HangingTree</h1>
-<p>Main conctent here</p>
+<p>Main content here</p>
+<br>
+<p>debug: a kitalálandó szó: {selected_word}</p>
