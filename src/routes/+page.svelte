b6d46267284afa8e9853f84f5a30b9aa6e909af4
6gDav <6gdavprog0402@gmail.com>
2026-09-22 15:00:14 +0200

    feat/fix: fix onclick sintax/add basic submit logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,23 +9,35 @@
         "developer",
         "algorithm",
         "function",
-        "variable"
+        "variable",
     ];
 
     let selected_word: string = words[Math.floor(Math.random() * words.length)];
 
     let inputChar: string = $state<string>("");
 
-    function submitCharacter() {
-        console.log("Submit clicked")
-    }
+    let errorCounter: number = 0;
 
+    function submitCharacter() {
+        inputChar = ""
+        if (inputChar) {
+            const isSingleBitRegex: boolean = /^[01]$/.test(inputChar);
+
+            if (isSingleBitRegex) {
+                if (selected_word.includes(inputChar)) {
+                    //succesfull logic
+                } else {
+                    errorCounter++;
+                }
+            }
+        }
+    }
 </script>
 
 <h1 style="font-size: 55px;">HangingTree</h1>
 <p>Main content here</p>
-<br>
+<br />
 <p>debug: a kitalálandó szó: {selected_word}</p>
 
-<input type="text" maxlength="1" bind:value={inputChar}> <br>
-<button onclick={() => submitCharacter}>Submint your try</button>
+<input type="text" maxlength="1" bind:value={inputChar} /> <br />
+<button on:click={submitCharacter}>Submit your try</button>
